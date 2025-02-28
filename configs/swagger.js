a604d6e8f5c8001c8af3c2373e136951ddccef8d
SaulD28 <edidonis42@gmail.com>
2025-02-28 02:17:53 -0600

    Se inicializo el servidor

---
+++ configs/swagger.js
@@ -0,0 +1,32 @@
+
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options ={
+    swaggerDefinition:{
+        openapi:"3.0.0",
+        info:{
+            title: "Coperex API",
+            version: "1.0.0",
+            description: "API para gestion de empresas y clientes para Coperex",
+            contact:{
+                name: "Edison Donis",
+                email: "edidonis42@gmail.com"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3001/apiCoperex/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/auth.routes.js",
+        "./src/user/admin.routes.js",
+
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(options)
+
+export { swaggerDocs, swaggerUi}
---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,37 @@
+`user strict`
+
+import jwt from "jsonwebtoken"
+
+export const validateJWT = async (req, res, next) =>{
+    try{
+        let token = req.body.token || req.query.token || req.headers["authorization"]
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "No se proporcionó un token en la petición"
+            })
+        }
+
+        token = token.replace(/^Bearer\s+/, "");
+
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
+        const admin = await Admin.findById(uid)
+
+        if(!admin){
+            return res.status(400).json({
+                success: false,
+                message: "Administrador no existe en la Base de Datos"
+            })
+        }
+
+        req.admin = admin
+        next()
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al validar el token",
+            error: err.message
+        })
+    }
+}
